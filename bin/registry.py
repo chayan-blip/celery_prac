ba1fba29e884dfdb82f4a7800bdd1d02a2022760
Chayan Pathak <chayanpathak2013@gmail.com>
2022-09-19 21:07:04 +0530

    Added comments to registry function

--- bin/registry.py
+++ bin/registry.py
@@ -2,10 +2,10 @@ from crunchy import discovery
 from UserDict import UserDict
 
 class NotRegistered(Exception):
-    """The task is not registered."""
+    """The task is not registered.""" ## not implemented
 
 class AlreadyRegistered(Exception):
-    """The task is already registered"""
+    """The task is already registered""" ## not implemented
 
 class TaskRegistry(UserDict):
     """Site registry for tasks"""
@@ -14,32 +14,32 @@ class TaskRegistry(UserDict):
     NotRegistered     = NotRegistered
 
     def __init__(self):
-        self.data = {}
+        self.data = {}   ## initialize
 
     def autodiscover(self):
         discovery.autodiscover()
 
     def register(self, task_name, task_func):
-        if task_name in self.data:
-            raise self.AlreadyRegistered(
+        if task_name in self.data: ## two tasks cannot have same name --> why ? 
+            raise self.AlreadyRegistered( ## name in terms of random uuid ?
                 "Task with name %s is already registered" % task_name
             )
 
-        self.data[task_name] = task_func
+        self.data[task_name] = task_func  ## register the task in the ledger
 
     def unregister(self, task_name):
-        if task_name not in self.data:
-            raise self.NotRegistered(
+        if task_name not in self.data:  ## error task we are trying to remove 
+            raise self.NotRegistered(   ## does not exist
                 "Task with name %s is not registered" % task_name)
         del self.data[task_name]
 
     def get_all(self, task_name):
-        """Get all the tasks by name"""
+        """Get all the tasks by name"""  
         return self.data
 
     def get_task(self, task_name):
         """Get task by name"""
-        return self.data[task_name]
+        return self.data[task_name]  ### seek a particular task
 
 """This is the global task registry."""
 tasks = TaskRegistry()
