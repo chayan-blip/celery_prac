9beb68747661367c40a900dfc04af33576b3d639
Chayan Pathak <chayanpathak2013@gmail.com>
2022-09-20 09:35:29 +0530

    Added comments to task.py

--- bin/task.py
+++ bin/task.py
@@ -8,13 +8,13 @@ def delay_task(task_name, **kwargs):
     #if task_name not in tasks:
     #   raise tasks.NotRegistered("Task with name %s not registered in the 
     #   task registry" %(task_name))
-    publisher = TaskPublisher(connection=DjangoAMPQConnection)
-    task_id = publisher.delay_task(task_name, **kwargs)
-    publisher.close()
+    publisher = TaskPublisher(connection=DjangoAMPQConnection) ## create a task publisher
+    task_id = publisher.delay_task(task_name, **kwargs) ## publisher create task
+    publisher.close() ## close publisher
     return task_id
 
 def discard_all():
-    consumer = TaskConsumer(connection=DjangoAMPQConnection)
-    discarded_count = consumer.discard_all()
-    consumer.close()
-    return discarded_count
+    consumer = TaskConsumer(connection=DjangoAMPQConnection) ## create consumer
+    discarded_count = consumer.discard_all() ## before closing consumer empty queue
+    consumer.close()        ## close consumer
+    return discarded_count ## message back number
