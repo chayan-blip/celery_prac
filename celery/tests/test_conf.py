60ae26a79e58160f85e8d4ddca9723e9ba6ba8b2
Chayan Pathak <chayanpathak2013@gmail.com>
2022-09-29 08:24:25 +0530

    Added comments

--- celery/tests/test_conf.py
+++ celery/tests/test_conf.py
@@ -2,6 +2,7 @@ import unittest
 from celery import conf
 from django.conf import settings
 
+## initialize setting variable
 SETTING_VARS = (
     ("CELERY_AMPQ_CONSUMER_QUEUE", "AMPQ_CONSUMER_QUEUE",
         "DEFAULT_AMPQ_CONSUMER_QUEUE"),
@@ -27,17 +28,17 @@ class TestConf(unittest.TestCase):
 
     def assertDefaultSettting(self, setting_name, result_var, default_var):
         if hasattr(settings,setting_name):
-            self.assertEquals(getattr(conf, result_var),
-                              getattr(settings, setting_name),
+            self.assertEquals(getattr(conf, result_var),  ## check whether the conf setting
+                              getattr(settings, setting_name), ## is written to variable
                               "Overwritten setting %s is written to %s"
                                 %(setting_name, result_var))
         else:
-            self.assertEqual(getattr(conf, default_var),
-                             getattr(conf,result_var),
+            self.assertEqual(getattr(conf, default_var), ## if not assigned then
+                             getattr(conf,result_var),  ## variable has default value
                              "Default setting %s is written to %s"
                              %(default_var, result_var))
         
         def test_configuration_cls(self):
-            for setting_name, result_var, default_var in SETTING_VARS:
+            for setting_name, result_var, default_var in SETTING_VARS: ## check for all settings
                 self.assertDefaultSetting(setting_name, result_var, default_var)
             self.assertTrue(isinstance(conf.DAEMON_LOG_LEVEL, int))
--- celery/tests/test_discovery.py
+++ celery/tests/test_discovery.py
@@ -7,12 +7,13 @@ from celery.task import tasks
 class TestDiscovery(unittest.TestCase):
 
     def assertDiscovery(self):
-        apps = autodiscover()
-        self.assertTrue(apps)
-        tasks.autodiscover()
-        self.assertTrue("c.unittest.SomeAppTask" in tasks)
-        self.assertEquals(task["c.unittest.SomeAppTask"].run(), 42)
+        apps = autodiscover() ## start discovery
+        self.assertTrue(apps) ## check that autodiscovery yielded true for some apps
+        ## that is tasks.py existed for at least some of the apps installed in django
+        tasks.autodiscover()   
+        self.assertTrue("c.unittest.SomeAppTask" in tasks) # try to run the tasks in the registry
+        self.assertEquals(tasks["c.unittest.SomeAppTask"].run(), 42) ## check if successfully run
     
     def test_discovery(self):
-        if "someapp" in settings.INSTALLED_APPS:
+        if "someapp" in settings.INSTALLED_APPS: ## run discovery
             self.assertDiscovery()
