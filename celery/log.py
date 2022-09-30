ae45965f580b70c3537236a1a4546ae69543a20a
Chayan Pathak <chayanpathak2013@gmail.com>
2022-09-30 09:06:06 +0530

    Updated logger code

--- celery/log.py
+++ celery/log.py
@@ -2,17 +2,19 @@ import multiprocessing
 import os
 import time
 import logging
-from crunchy.conf import LOG_FORMAT, DAEMON_LOG_LEVEL
+from celery.conf import LOG_FORMAT, DAEMON_LOG_LEVEL
 
-def setup_logger(loglevel=DAEMON_LOG_LEVEL, logfile=None, format=LOG_FORMAT):
+def setup_logger(loglevel=DAEMON_LOG_LEVEL, logfile=None, format=LOG_FORMAT, **kwargs):
     """"Setup the ``multiprocessing`` logger. If ``logfile`` is not specified,
     ``stderr`` is used.
     Returns the logger object.
     """
     logger = multiprocessing.get_logger() ## create a logger object using multiprocessing lib
     if logfile: ## if the log file already exists then create a log handler in order to log all events
-        log_file_handler = logging.FileHandler(logfile) ## pass the log file location and create a file handler
-        ## which will use that file as the output stream for the messages emanating from the daemon
+        if hasattr(logfile, "write"):
+            log_file_handler = logging.StreamHandler(logfile)
+        else:
+            log_file_handler = logging.FileHandler(logfile)
         formatter = logging.Formatter(format) ## take the log format obtained from django settings
         ## and use it to specify the format in which the log file will be printed
         ## here we are using time, process number, what type of message (warning/error etc) and message itself
@@ -25,11 +27,20 @@ def setup_logger(loglevel=DAEMON_LOG_LEVEL, logfile=None, format=LOG_FORMAT):
     return logger ## pass the logger object
 
 def emergency_error(logfile, message):
-    logfh = open(logfile, "a") ## creates a file handler for emergency purposes. when logger is not working ?
-    logfh.write("[%()s : FATAL/%(pid)d]: %()s \n" % {
+    """Emergency error logging, for when there's no standard file
+    descriptors open because the process has been daemonized or for
+    some other reason"""
+    logfh_needs_to_close = False
+    if hasattr(logfile, "write"):
+        logfh = logfile
+    else:
+        logfh = open(logfile, "a")
+        logfh_needs_to_close = True
+    logfh.write("[%(asctime)s : FATAL/%(pid)d]: %(message)s \n" % {
         "asctime" : time.asctime(), ## note the present time , convert to string and write to message
         "pid":os.getpid(), ## get the other details like pid, and message , and write them
         ## basically simulating the task done by the logger using a file handler
         "message":message
     })
-    logfh.close() ## safely close the file
+    if logfh_needs_to_close:
+        logfh.close() ## safely close the file
