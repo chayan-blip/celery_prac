a53e90323dad51fa1c07b1ea3beb6667ba14b27a
chayanpathak <chayanpathak2013@gmail.com>
2026-05-16 20:51:47 +0530

    Removed bug of self being nested called twice

--- task/task.py
+++ task/task.py
@@ -2,4 +2,13 @@ class Task:
     """
     Base class for all tasks
     """
-    pass
+
+    # defer execution to run method which is
+    # implemented in TaskClass called from decorator
+    # refer to decorator.py file
+
+    def __call__(self, *args, **kwargs):
+        return self.run(*args,**kwargs)
+
+    def run(self, *args, **kwargs):
+        raise NotImplementedError("Task does not run directly . Should be called from decorator")
