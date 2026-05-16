719a8796c14bc7c3913b0f3295f53e58b4e02f87
chayanpathak <chayanpathak2013@gmail.com>
2026-05-16 20:09:44 +0530

    Added understanding comments to the decorator file

--- celery_prac/decorator.py
+++ celery_prac/decorator.py
@@ -1,11 +1,25 @@
 from functools import wraps
 
+from task.task import Task
+
 
 def task(*args, **kwargs):
     """
-        create task class out of any callable
+        refer the comments below
     """
     if len(args) == 1 and callable(args[0]):
-        return args[0](kwargs)
+        # Create a function of the name
+        # calling the Task decorator
+        func = args[0]
+
+        # Create a run method which calls the actual function
+        # with the arguments which were given to the actual function such as add(2,3)
+        def run(self, *args, **kwargs):
+            return func(*args, **kwargs)
+
+        # Create a new object of Task Class type from the function passed above
+        TaskClass = type(func.__name__, (Task,), {"run": run})
+
+        return TaskClass
     else:
         raise TypeError("only @task decorator supported")
